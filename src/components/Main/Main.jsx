b80af68192c9e6647aec139b18b2d31b83510cd3
Alexander Vasilevsky <vasilevskiysasha1@gmail.com>
2022-11-30 23:39:46 +0300

    Add lib Frame + new animation Main.jsx using this library

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,20 +1,104 @@
+import { motion } from "framer-motion";
 import s from "./main.module.scss";
 import avatar from "../../assets/images/alex.jpg";
+
+const textAnimation = {
+  hidden: {
+    x: -100,
+    opacity: 0,
+  },
+  visible: (custom) => ({
+    x: 0,
+    opacity: 1,
+    transition: { delay: custom * 0.5, duration: 1 },
+  }),
+};
+
+const dotAnimation = {
+  hidden: {
+    scale: 0,
+    opacity: 0,
+  },
+  visible: (custom) => ({
+    scale: 1,
+    opacity: 1,
+    transition: { delay: custom * 0.25, duration: 1 },
+  }),
+};
 
 const Main = () => {
   return (
     <div className={s.container} id="main">
-      <div className={s.block}>
-        <img className={s.block_photo} src={avatar} alt="avatar" />
+      <motion.div
+        animate={{
+          scale: [1, 0.9, 1.1, 1],
+        }}
+        className={s.block}
+      >
+        <motion.img
+          initial={{ opacity: 0, scale: 0.5 }}
+          whileInView={{
+            opacity: 1,
+            scale: 1,
+            transition: {
+              delay: 0.2,
+              x: { duration: 0.3 },
+              default: { ease: "linear" },
+            },
+          }}
+          className={s.block_photo}
+          src={avatar}
+          alt="avatar"
+        />
         <div className={s.block_text}>
-          <div className={s.block_text_name}>Alexander Vasilevsky</div>
-          <div className={s.block_text_prof}>FRONT-END DEVELOPER(REACT)</div>
+          <motion.div
+            custom={0.5}
+            initial="hidden"
+            whileInView="visible"
+            variants={textAnimation}
+            className={s.block_text_name}
+          >
+            Alexander Vasilevsky
+          </motion.div>
+          <motion.div
+            custom={0.9}
+            initial="hidden"
+            whileInView="visible"
+            variants={textAnimation}
+            className={s.block_text_prof}
+          >
+            FRONT-END DEVELOPER(REACT)
+          </motion.div>
         </div>
-        <div className={s.brown_dot + " " + s.leftUpCorner}></div>
-        <div className={s.brown_dot + " " + s.leftDownCorner}></div>
-        <div className={s.brown_dot + " " + s.rightUpCorner}></div>
-        <div className={s.brown_dot + " " + s.rightDownCorner}></div>
-      </div>
+        <motion.div
+          custom={2}
+          initial="hidden"
+          whileInView="visible"
+          variants={dotAnimation}
+          className={s.brown_dot + " " + s.leftUpCorner}
+        ></motion.div>
+        <motion.div
+          custom={3}
+          initial="hidden"
+          whileInView="visible"
+          variants={dotAnimation}
+          className={s.brown_dot + " " + s.leftDownCorner}
+        ></motion.div>
+        <motion.div
+          custom={4}
+          initial="hidden"
+          whileInView="visible"
+          variants={dotAnimation}
+          className={s.brown_dot + " " + s.rightUpCorner}
+        ></motion.div>
+        <motion.div
+          custom={5}
+          initial="hidden"
+          whileInView="visible"
+          variants={dotAnimation}
+          className={s.brown_dot + " " + s.rightDownCorner}
+        ></motion.div>
+      </motion.div>
     </div>
   );
 };
