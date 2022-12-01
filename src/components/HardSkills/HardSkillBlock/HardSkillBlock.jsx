bbea87238e5db590855c4f0a52bd16038df423c2
Alexander Vasilevsky <vasilevskiysasha1@gmail.com>
2022-12-01 22:29:20 +0300

    Added Motion HardSkills

--- src/components/HardSkills/HardSkillBlock/HardSkillBlock.jsx
+++ src/components/HardSkills/HardSkillBlock/HardSkillBlock.jsx
@@ -1,11 +1,27 @@
+import { motion } from "framer-motion";
 import s from "./hardSkillBlock.module.scss";
+
+const animLongSkill = {
+  hidden: {
+    opacity: 0,
+  },
+
+  visible: {
+    opacity: 1,
+  },
+};
 
 const HardSkillBlock = (props) => {
   const cn = require("classnames");
   const { classScss, title, icon, text } = props.skill;
   return (
     <>
-      <div className={cn(s.block, s[classScss])}>
+      <motion.div
+        variants={animLongSkill}
+        initial={"hidden"}
+        whileInView={"visible"}
+        className={cn(s.block, s[classScss])}
+      >
         <div className={s.block_svg}>
           <img
             src={icon}
@@ -20,7 +36,7 @@ const HardSkillBlock = (props) => {
             return <p key={index}>{txt}</p>;
           })}
         </div>
-      </div>
+      </motion.div>
     </>
   );
 };
--- src/components/HardSkills/HardSkills.jsx
+++ src/components/HardSkills/HardSkills.jsx
@@ -1,13 +1,37 @@
+import { motion } from "framer-motion";
 import HardSkillBlock from "./HardSkillBlock/HardSkillBlock";
 import s from "./hardSkills.module.scss";
 import { hardSkills, shortHardSkills } from "../../data/hardSkills";
+
+const animShortSkill = {
+  hidden: {
+    opacity: 0,
+  },
+
+  visible: (i) => ({
+    opacity: 1,
+    transition: {
+      delay: i * 0.1,
+    },
+  }),
+};
 
 const HardSkills = () => {
   return (
     <div className={s.container} id="hardSkills">
       <div className={s.shortSkills}>
         {shortHardSkills.map((elem, idx) => {
-          return <div key={idx}>{elem}</div>;
+          return (
+            <motion.div
+              variants={animShortSkill}
+              initial={"hidden"}
+              whileInView={"visible"}
+              custom={idx}
+              key={idx}
+            >
+              {elem}
+            </motion.div>
+          );
         })}
       </div>
       <div>
